357895b9a1528570807f3662c9383142152ed0d2
theo gandara <theogandara@gmail.com>
2022-01-27 11:04:22 -0300

    feat: mvp form register

--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -1,7 +1,112 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
+
+import { Background, Modal, Form, Title } from "./style";
+
 export const Register = () => {
+  const [volunter, setVolunter] = useState(true);
+
+  const formSchema = yup.object().shape({
+    name: yup.string().required("Informe o seu nome").min(3, "Nome inválido"),
+    email: yup.string().required("Email obrigatório").email("Email inválido"),
+    password: yup.string().required("Insira uma senha").min(6, "Senha fraca"),
+    confirm_password: yup.string().required("Insira uma senha"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <Background>
+      <Modal>
+
+        {volunter ? (
+          <>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+            <Title>Cadastro de usuário</Title>
+              <input
+                type="text"
+                placeholder="Digite seu nome"
+                {...register("name")}
+              />
+              <p>{errors.name?.message}</p>
+
+              <input
+                type="text"
+                placeholder="Digite seu email"
+                {...register("email")}
+              />
+              <p>{errors.email?.message}</p>
+
+              <input
+                type="password"
+                placeholder="Digite sua senha"
+                {...register("password")}
+              />
+              <p>{errors.password?.message}</p>
+
+              <input
+                type="password"
+                placeholder="Confirme sua senha"
+                {...register("confirm_password")}
+              />
+              <p>{errors.confirm_password?.message}</p>
+
+              <button type="submit">Cadastrar</button>
+            </Form>
+          </>
+        ) : (
+          <>
+            
+            <Form onSubmit={handleSubmit(onSubmit)}>
+            <Title>Cadastro de instituição</Title>
+              <input
+                type="text"
+                placeholder="Digite seu nome"
+                {...register("name")}
+              />
+              <p>{errors.name?.message}</p>
+
+              <input
+                type="text"
+                placeholder="Digite seu email"
+                {...register("email")}
+              />
+              <p>{errors.email?.message}</p>
+
+              <input
+                type="password"
+                placeholder="Digite sua senha"
+                {...register("password")}
+              />
+              <p>{errors.password?.message}</p>
+
+              <input
+                type="password"
+                placeholder="Confirme sua senha"
+                {...register("confirm_password")}
+              />
+              <p>{errors.confirm_password?.message}</p>
+
+              <button type="submit">Cadastrar</button>
+            </Form>
+          </>
+        )}
+
+        <div>
+          <button onClick={() => setVolunter(true)}>voluntário</button>
+          <button onClick={() => setVolunter(false)}>instituição</button>
+        </div>
+      </Modal>
+    </Background>
   );
 };
---
+++ src/Pages/Register/style.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  min-height: 100vh;
+  width: 100vw;
+  background-color: grey;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Modal = styled.div`
+  width: 90vw;
+  height: 85vh;
+  border-radius: 20px;
+  background-color: #fff;
+
+  display: flex;
+  align-items: center;
+  align-content: space-around;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+export const Form = styled.form`
+
+height: 50%;
+width: 70%;
+max-width: 350px;
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+
+`
+export const Title = styled.h2`
+
+width: 90%;
+font-size: 20px;
+`
