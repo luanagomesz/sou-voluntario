6c270aea97459cab3e7e7750714ba4a90f60c375
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2022-01-31 16:22:43 -0300

    feature: created and applyed context to load subscribe events by logged user

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Providers } from "./Contexts";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Providers>
+        <App />
+      </Providers>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
