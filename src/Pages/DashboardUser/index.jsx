141dc2f51a51062ab3a019b5eafd6c312873d022
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2022-01-31 13:06:30 -0300

    update: change z-index position of fake header

--- src/Pages/DashboardUser/index.jsx
+++ src/Pages/DashboardUser/index.jsx
@@ -2,13 +2,34 @@ import {
   FakeHeader,
   Main,
   EventsTypeContainer,
-  EventsSearchContainer,
+  EventsList,
+  EventsListContainer,
 } from "./style";
 import { EventTypeSelector } from "../../Components/EventTypeSelector";
 import { EventUserCard } from "../../Components/EventUserCard";
-import { EventsSearch } from "../../Components/EventsSearch";
 
 export const DashboardUser = () => {
+  const array = new Array(
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+  );
+
   return (
     <>
       <FakeHeader />
@@ -16,14 +37,13 @@ export const DashboardUser = () => {
         <EventsTypeContainer className="events__type__container">
           <EventTypeSelector />
         </EventsTypeContainer>
-        <EventsSearchContainer>
-          <EventsSearch />
-        </EventsSearchContainer>
-        <section className="events__list__container">
-          <ul className="events__list">
-            <EventUserCard />
-          </ul>
-        </section>
+        <EventsListContainer className="events__list__container">
+          <EventsList className="events__list">
+            {array.map((_) => (
+              <EventUserCard />
+            ))}
+          </EventsList>
+        </EventsListContainer>
       </Main>
     </>
   );
--- src/Pages/DashboardUser/style.js
+++ src/Pages/DashboardUser/style.js
@@ -5,12 +5,21 @@ export const FakeHeader = styled.header`
   height: 130px;
   background-color: #fff;
   box-shadow: 0 4px 20px 0px rgba(0, 0, 0, 0.1);
+  position: relative;
+  z-index: 2;
 `;
 
 export const Main = styled.main`
   width: 100%;
-  height: calc(100vh - 130px);
+  min-height: calc(100vh - 130px);
+
+  padding-bottom: 50px;
+
   background-color: var(--ligthblue);
+  > * {
+    max-width: 1440px;
+    margin: 0 auto;
+  }
 `;
 
 export const EventsTypeContainer = styled.section`
@@ -23,4 +32,18 @@ export const EventsSearchContainer = styled.section`
   width: 100%;
   height: 100px;
   padding: 30px 20px 15px;
+`;
+
+export const EventsListContainer = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 0 20px;
+`;
+
+export const EventsList = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 40px;
 `;
