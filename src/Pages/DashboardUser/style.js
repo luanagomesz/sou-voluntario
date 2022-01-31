363f13c6fe5686eaabe5fb7eaf8b4992dac10f3b
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2022-01-31 16:29:29 -0300

    fix: removed horizontal scroll

--- src/Pages/DashboardUser/style.js
+++ src/Pages/DashboardUser/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const FakeHeader = styled.header`
-  width: 100%;
+  width: 100vw;
   height: 130px;
   background-color: #fff;
   box-shadow: 0 4px 20px 0px rgba(0, 0, 0, 0.1);
@@ -10,40 +10,36 @@ export const FakeHeader = styled.header`
 `;
 
 export const Main = styled.main`
-  width: 100%;
+  width: 100vw;
   min-height: calc(100vh - 130px);
 
   padding-bottom: 50px;
 
   background-color: var(--ligthblue);
   > * {
-    max-width: 1440px;
     margin: 0 auto;
+    max-width: 1440px;
   }
 `;
 
-export const EventsTypeContainer = styled.section`
+export const EventsStatusContainer = styled.section`
   width: 100%;
   height: 100px;
-  padding: 30px 20px 15px;
-`;
-
-export const EventsSearchContainer = styled.section`
-  width: 100%;
-  height: 100px;
-  padding: 30px 20px 15px;
+  padding-top: 30px;
+  padding-bottom: 15px;
 `;
 
 export const EventsListContainer = styled.section`
   width: 100%;
   display: flex;
   justify-content: center;
-  padding: 0 20px;
+  padding-top: 15px;
 `;
 
 export const EventsList = styled.ul`
+  width: 100%;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   flex-wrap: wrap;
   gap: 40px;
 `;
