a574679c6443506d86a14647663f957697897bf8
Luana Gomes <88343153+luanagomesz@users.noreply.github.com>
2022-02-03 15:10:23 -0300

    Revert "conflicts resolved"

--- src/Contexts/index.jsx
+++ src/Contexts/index.jsx
@@ -2,11 +2,9 @@ import { AuthProvider } from "./Auth";
 import { UserEventsProvider } from "./UserEvents";
 import { RegisterEventsProvider } from "./RegisterEvents";
 import { EventsPageProvider } from "./EventPage";
-import { BurgerProvider } from "./Burger/toggle";
 
 export const Providers = ({ children }) => {
   return (
-<<<<<<< HEAD
     <AuthProvider>
       <EventsPageProvider>
         <RegisterEventsProvider>
@@ -14,16 +12,5 @@ export const Providers = ({ children }) => {
         </RegisterEventsProvider>
       </EventsPageProvider>
     </AuthProvider>
-=======
-    <BurgerProvider>
-      <AuthProvider>
-        <EventsPageProvider>
-          <RegisterEventsProvider>
-            <UserEventsProvider>{children}</UserEventsProvider>;
-          </RegisterEventsProvider>
-        </EventsPageProvider>
-      </AuthProvider>
-    </BurgerProvider>
->>>>>>> 4d1c6d7af000c66c5f7fe007b8cb1185c1f084b6
   );
 };
